7d3a009e4306b517af5b6ad972a00619eb35b352
Ismael Correia da Silva <ismael_correia@hotmail.com>
2023-03-03 13:42:00 -0300

    Start of project

---
+++ SendEmail/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SendEmail.Models;
+using System.Diagnostics;
+
+namespace SendEmail.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Email email)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                RedirectToAction("SuccessSend", "Home");
+            }
+            return View();
+
+        }
+    }
+}
---
+++ SendEmail/Models/Email.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SendEmail.Models
+{
+    public class Email
+    {
+        [Required(ErrorMessage = "Is Required")]
+        public string EmailAddress { get; set; }
+        [Required(ErrorMessage = "Is Required")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "Is Required")]
+        public string Message { get; set; }
+    }
+}
